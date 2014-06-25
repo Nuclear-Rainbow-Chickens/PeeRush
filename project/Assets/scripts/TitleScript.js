210e01594d3840239f1ccf95b92fd2249db61dd8
supermiu <jeffersonlee2000@gmail.com>
2014-06-24 21:05:39 -0700

    built again

---
+++ project/Assets/scripts/TitleScript.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	if(Input.GetKeyUp(KeyCode.A))
+	{
+		Application.LoadLevel(1);
+	}
+	if(Input.GetKeyUp(KeyCode.S))
+	{
+		Application.ExternalCall("home");
+	}
+	
+}
